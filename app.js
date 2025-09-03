7b5be4efeeb3a9ace048ddcf5f91640ff6f06658
colduniqolo <colduniqolo@gmail.com>
2025-09-04 03:47:30 +0800

    refactor: tidy sample code

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-// auto-update: 2025-08-29T16:20:00.507738Z
+// auto-update: 2025-09-03T19:50:06.418468Z
 function add(){ const i=document.getElementById('i'); if(!i.value) return;
  const li=document.createElement('li'); li.textContent=i.value; list.appendChild(li); i.value=''; }
